4db2462ddbafd113e068f0ff21ae09525365929d
Andrey Lozovoy <lozovoya@gmail.com>
2021-09-28 22:04:14 +0300

    getsnapshot with channels

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -9,6 +9,4 @@ type Call interface {
 	CallToCache(ctx context.Context, call *model.Call) error
 	CallFromCache(ctx context.Context, queueID, callID string) error
 	GetCallsSnapshot(ctx context.Context) ([]*model.Call, error)
-	GetCallsSnapshot2(ctx context.Context) ([]*model.Call, error)
-	GetCallsSnapshot3(ctx context.Context) ([]*model.Call, error)
 }
