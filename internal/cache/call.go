c087f63659088f9c951fc06b1b24f738acd0e4e2
Andrey Lozovoy <lozovoya@gmail.com>
2021-09-28 22:03:42 +0300

    getsnapshot with channels

--- internal/cache/call.go
+++ internal/cache/call.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -65,32 +66,78 @@ func (c callCache) GetCallsSnapshot(ctx context.Context) ([]*model.Call, error) 
 			log.Println(err)
 		}
 	}()
-	var calls []*model.Call
-	var cursor = 0
-	for {
-		data, err := redis.Values(redis.DoWithTimeout(conn, time.Millisecond*2000, "SCAN", cursor))
-		if err != nil {
-			return nil, fmt.Errorf("GetCallsSnapshot %w", err)
-		}
-		cursor, _ = redis.Int(data[0], nil)
-		keys, _ := redis.Strings(data[1], nil)
 
-		for _, key := range keys {
-			var call model.Call
-			data, err := redis.Bytes(redis.DoWithTimeout(conn, time.Millisecond*2000, "GET", key))
-			if err != nil {
-				return nil, fmt.Errorf("GetCallsSnapshot %w", err)
-			}
-			err = json.Unmarshal(data, &call)
-			if err != nil {
-				return nil, fmt.Errorf("GetCallsSnapshot %w", err)
-			}
-			calls = append(calls, &call)
+	var calls []*model.Call
+	wg := sync.WaitGroup{}
+	resultCh := make(chan []*model.Call)
+	keysCh := make(chan []string)
+
+	go func() {
+		for r := range resultCh {
+			log.Printf("recording calls to slice: %d", len(calls))
+			result := r
+			calls = append(calls, result...)
 		}
-		if cursor == 0 {
-			break
+		log.Println("recording calls is finished")
+
+	}()
+
+	go func() {
+		var cursor = 0
+		var counter = 1000
+		for {
+			log.Printf("redis works, counter %d", counter)
+			data, err := redis.Values(redis.DoWithTimeout(conn, time.Millisecond*2000, "SCAN", cursor, "COUNT", counter))
+			if err != nil {
+				fmt.Errorf("GetCallsSnapshot %w", err)
+				close(keysCh)
+			}
+			cursor, _ = redis.Int(data[0], nil)
+			keys, _ := redis.Strings(data[1], nil)
+			log.Printf("keys length: %d", len(keys))
+			keysCh <- keys
+			if cursor == 0 {
+				close(keysCh)
+				break
+			}
 		}
 		log.Println("redis done")
+	}()
+
+	i := 0
+	for keys := range keysCh {
+		wg.Add(1)
+		i++
+		log.Printf("start gr %d", i)
+		go func(keys []string, wg *sync.WaitGroup) {
+			conn, err := c.pool.GetContext(ctx)
+			if err != nil {
+				fmt.Errorf("GetCallsSnapshot %w", err)
+			}
+			defer conn.Close()
+			var calls []*model.Call
+			for _, key := range keys {
+				//log.Printf("searching keys in redis: %s", key)
+				var call model.Call
+				data, _ := redis.Bytes(redis.DoWithTimeout(conn, time.Millisecond*2000, "GET", key))
+				if err != nil {
+					fmt.Errorf("GetCallsSnapshot %w", err)
+				}
+				err = json.Unmarshal(data, &call)
+				if err != nil {
+					fmt.Errorf("GetCallsSnapshot %w", err)
+				}
+				calls = append(calls, &call)
+			}
+			log.Println("send result to channel")
+			resultCh <- calls
+			wg.Done()
+		}(keys, &wg)
 	}
+
+	log.Println("wait for gr")
+	wg.Wait()
+	close(resultCh)
+
 	return calls, nil
 }
